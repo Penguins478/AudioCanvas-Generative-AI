ca626841d9b01f5cd6b55155959c419a86046bbf
Ryan <ryankwong478@gmail.com>
2023-07-20 08:27:24 -0700

    added a dp cache

--- backend/api/views.py
+++ backend/api/views.py
@@ -5,22 +5,46 @@ from .serializers import AudioRecordSerializer
 import os
 import openai
 import warnings
+from django.core.cache import cache
+from rest_framework import status
 
 warnings.filterwarnings("ignore")
 
 openai.api_key = "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"
 
+# @api_view(['POST'])
+# def process_audio(request):
+#     if request.method == 'POST':
+#         serializer = AudioRecordSerializer(data=request.data)
+#         # print(serializer.is_valid())
+#         # print(request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             audio_record = serializer.instance
+#             audio_path = audio_record.audio_file.path
+#             transcript, image_url = whisper_transcribe(audio_path)
+#             audio_record.transcription = transcript
+#             audio_record.image_url = image_url
+#             audio_record.save()
+#             response_serializer = AudioRecordSerializer(audio_record)
+#             return Response(response_serializer.data)
+#         return Response(serializer.errors, status=400)
+#     return Response({'message': 'Invalid request'}, status=400)
+
 @api_view(['POST'])
 def process_audio(request):
     if request.method == 'POST':
         serializer = AudioRecordSerializer(data=request.data)
-        # print(serializer.is_valid())
-        # print(request.data)
+
+        # Get the 'use_cache' value from the request data
+        use_cache = request.data.get('use_cache', False)
+        # print(use_cache)
+
         if serializer.is_valid():
             serializer.save()
             audio_record = serializer.instance
             audio_path = audio_record.audio_file.path
-            transcript, image_url = whisper_transcribe(audio_path)
+            transcript, image_url = whisper_transcribe(audio_path, use_cache)
             audio_record.transcription = transcript
             audio_record.image_url = image_url
             audio_record.save()
@@ -55,10 +79,62 @@ def dall_e_api(dalle_prompt):
     image_url = dalle_response['data'][0]['url']
     return image_url
 
-def whisper_transcribe(audio):
+# def whisper_transcribe(audio):
+#     os.rename(audio, audio + '.wav')
+#     audio_file = open(audio + '.wav', "rb")
+#     transcript = openai.Audio.transcribe("whisper-1", audio_file)
+#     dalle_prompt = chatgpt_api(transcript["text"])
+#     image_url = dall_e_api(dalle_prompt)
+#     return transcript["text"], image_url
+
+# def whisper_transcribe(audio):
+#     os.rename(audio, audio + '.wav')
+#     audio_file = open(audio + '.wav', "rb")
+
+#     # Get the audio transcription
+#     transcript = openai.Audio.transcribe("whisper-1", audio_file)["text"]
+
+#     # Check if the image URL is already cached
+#     image_cache_key = f"image_url:{transcript}"
+#     image_url = cache.get(image_cache_key)
+
+#     if image_url is None:
+#         # Get the DALL-E prompt using the transcript
+#         dalle_prompt = chatgpt_api(transcript)
+
+#         # Generate the image URL using the DALL-E prompt
+#         image_url = dall_e_api(dalle_prompt)
+
+#         # Cache the image URL for future use
+#         cache.set(image_cache_key, image_url)
+
+#     return transcript, image_url
+
+def whisper_transcribe(audio, use_cache):
     os.rename(audio, audio + '.wav')
     audio_file = open(audio + '.wav', "rb")
-    transcript = openai.Audio.transcribe("whisper-1", audio_file)
-    dalle_prompt = chatgpt_api(transcript["text"])
+
+    # Get the audio transcription
+    transcript = openai.Audio.transcribe("whisper-1", audio_file)["text"]
+    # print(use_cache)
+    # print(type(use_cache))
+    # Check if the image URL is already cached and the use_cache flag is True
+    if use_cache == 'true':
+        image_cache_key = f"image_url:{transcript}"
+        image_url = cache.get(image_cache_key)   
+        if image_url is not None:
+            return transcript, image_url
+
+    # if image_url is None:
+    # Get the DALL-E prompt using the transcript
+    dalle_prompt = chatgpt_api(transcript)
+
+    # Generate the image URL using the DALL-E prompt
     image_url = dall_e_api(dalle_prompt)
-    return transcript["text"], image_url
+
+    if use_cache == 'true':
+        # Cache the image URL for future use
+        cache.set(image_cache_key, image_url)
+
+    return transcript, image_url
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,18 @@ import React, { useState, useRef } from 'react';
 import axios from 'axios';
 
 function App() {
+
   const [isRecording, setIsRecording] = useState(false);
   const [audioBlob, setAudioBlob] = useState(null);
   const [transcription, setTranscription] = useState('');
   const [imageUrl, setImageUrl] = useState('');
+  const [useCache, setUseCache] = useState(false);
   const mediaRecorderRef = useRef(null);
   const audioPlayerRef = useRef(null);
+
+  const toggleCacheState = () => {
+    setUseCache(!useCache);
+  }
 
   const startRecording = () => {
     const mediaConstraints = { audio: true };
@@ -51,10 +57,11 @@ function App() {
       const formData = new FormData();
       formData.append('audio_file', audioBlob, 'recording.webm');
       // console.log(formData.get('audio_file'));
+      formData.append('use_cache', useCache); // Add the useCache state to the formData
 
       axios.post('http://localhost:8000/api/process-audio/', formData)
         .then(response => {
-          console.log('Response from server:', response.data);
+          //console.log('Response from server:', response.data);
           const { transcription, image_url } = response.data;
           setTranscription(transcription);
           setImageUrl(image_url);
@@ -64,6 +71,8 @@ function App() {
         });
     }
   };
+
+  // console.log(useCache);
 
   return (
     <div>
@@ -81,8 +90,27 @@ function App() {
         <button onClick={submitAudio} disabled={!audioBlob}>
           Submit Audio
         </button>
+
+
+       
+
       </div>
       <audio ref={audioPlayerRef} controls />
+
+      <div>
+      
+        <label>
+          <input
+            type="checkbox"
+            checked={useCache}
+            onChange={toggleCacheState}
+          />
+          Use Transcription/Image Cache
+        </label>
+
+
+      </div>
+      
       <div><p>Please wait a couple seconds after submitting the audio for processing time.</p></div>
       {transcription && (
         <div>
@@ -96,8 +124,11 @@ function App() {
           <img src={imageUrl} alt="Generated Image" />
         </div>
       )}
+      
     </div>
   );
 }
 
 export default App;
+
+
